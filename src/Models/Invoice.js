c8d07a9666bd9be62c4ec186cbede24557a71615
ronaldSWE <ronaldokuk462@gmail.com>
2026-04-21 21:57:17 +0100

    InvoicePro Server Update: Added sender and receiver address fields to Invoice model.

--- src/Models/Invoice.js
+++ src/Models/Invoice.js
@@ -3,8 +3,16 @@ import mongoose from "mongoose";
 const InvoiceSchema = new mongoose.Schema({
   invoiceID: { type: String, required: true, unique: true },
   date: { type: Date, required: true },
+  senderStreetAddress: { type: String, required: true },
+  senderCity: { type: String, required: true },
+  senderPostalCode: { type: Number, required: true },
+  senderCountry: { type: String, required: true },
   customerName: { type: String, required: true },
   customerEmail: { type: String },
+  receiverStreetAddress: { type: String, required: true },
+  receiverCity: { type: String, required: true },
+  receiverPostalCode: { type: Number, required: true },
+  receiverCountry: { type: String, required: true },
   items: [
     {
       description: { type: String, required: true },
